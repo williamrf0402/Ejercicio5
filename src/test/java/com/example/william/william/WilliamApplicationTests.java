97a87080741d9bbc03c51df0f32c7db887f422b5
williamrf0402 <135016972+williamrf0402@users.noreply.github.com>
2023-05-31 12:20:56 +0200

    Add files via upload

---
+++ src/test/java/com/example/william/william/WilliamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.william.william;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilliamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
